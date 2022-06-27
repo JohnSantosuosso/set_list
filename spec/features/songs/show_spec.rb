c9f36318448b1d29e5b88f2aa9b363998a9f4f06
John Santosuosso <john.santosuosso1@gmail.com>
2022-06-27 12:08:46 -0400

    add show method to songs_controller.rb, add test to show_spec.rb for finding and displaying artist and song names on page, add artist and song names to show.html.erb

--- spec/features/songs/show_spec.rb
+++ spec/features/songs/show_spec.rb
@@ -2,10 +2,30 @@ require 'rails_helper'
 
 RSpec.describe 'the songs show page' do
   it 'displays the song title' do
-    song = Song.create(title: "Chicken Fried", length: 28, play_count: 23248 )
+    artist = Artist.create(name: "Zac Brown")
+    song = artist.songs.create!(title: "Chicken Fried", length: 28, play_count: 23248 )
+    song_2 = artist.songs.create!(title: "Freedom", length: 49, play_count: 248 )
     visit "/songs/#{song.id}"
     # localhost: 3000/songs/1
+
+    expect(page).to have_content(song.title)
+    expect(page).to_not have_content(song_2.title)
   end
 
-  it 'displays the name of an artist for the song'
+  #As a visitor, when I visit /songs/1 (where 1 is the id of a song) I see that song's title and artist
+  #I do not see any of the other songs in my database
+
+  it 'displays the name of an artist for the song' do
+    artist = Artist.create(name: "Zac Brown")
+    song = artist.songs.create!(title: "Chicken Fried", length: 28, play_count: 23248 )
+    song_2 = artist.songs.create!(title: "Freedom", length: 49, play_count: 248 )
+    visit "/songs/#{song.id}"
+    # localhost: 3000/songs/1
+    save_and_open_page #capybara test/visual confirmation
+
+    expect(page).to have_content(artist.name)
+
+  end
+
+
 end
