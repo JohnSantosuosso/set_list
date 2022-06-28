fdd01f8fc7bb95acac2ea54c833f17263d227476
John Santosuosso <john.santosuosso1@gmail.com>
2022-06-28 10:19:02 -0400

    add destroy method and test

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -9,10 +9,27 @@ class ArtistsController < ApplicationController
   end
 
   def create
-    artist = Artist.create(artist_params)
+    artist = Artist.create(artist_params)  #we don't use instance variables when possible if we don't want to pass to view
     redirect_to "/artists"
   end
 
+  def edit
+    @artist = Artist.find(params[:id]) #we use instance variables when we want to pass to View
+  end
+
+  def update
+    artist = Artist.find(params[:id])
+    artist.update(artist_params)
+    redirect_to '/artists'
+  end
+
+  def destroy
+    artist = Artist.find(params[:id])
+    artist.destroy
+    redirect_to '/artists'
+  end
+
+private
   def artist_params
     params.permit(:name)
   end
